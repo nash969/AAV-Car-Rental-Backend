0492f5a22fd34801cee351f0a1b8bc6d026d9d7c
nash969 <jonashbernardino9@gmail.com>
2026-08-31 23:52:02 +0800

    Run database migrations during deployment

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -6,6 +6,8 @@ cd /var/www/html
 
 composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 
+php artisan migrate --force
+
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
